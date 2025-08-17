c7dc9b98837133f14bef8b38aa0b46caac6d8c6f
Pes <withanagepm21@gmail.com>
2025-08-17 11:56:37 +0530

    Add customer functionality

---
+++ src/main/java/controller/AddCustomerServlet.java
@@ -0,0 +1,31 @@
+package controller;
+
+import dto.CustomerDTO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import service.CustomerService;
+
+import java.io.IOException;
+
+@WebServlet({"/add-customer"})
+public class AddCustomerServlet extends HttpServlet {
+    private final CustomerService customerService = new CustomerService();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setName(request.getParameter("name"));
+        customerDTO.setEmail(request.getParameter("email"));
+        customerDTO.setAddress(request.getParameter("address"));
+        customerDTO.setTelephone(request.getParameter("telephone"));
+        boolean success = this.customerService.addCustomer(customerDTO);
+        if (success) {
+            response.sendRedirect(request.getContextPath() + "/success.jsp");
+        } else {
+            response.sendRedirect(request.getContextPath() + "/error.jsp");
+        }
+
+    }
+}
